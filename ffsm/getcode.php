c82ff5d6fc0df593df50117548d54e17d9a77187
shihao.jsh <shihao.jsh@alipay.com>
2020-07-27 16:09:29 +0800

    修改地址

--- ffsm/getcode.php
+++ ffsm/getcode.php
@@ -14,10 +14,10 @@ require 'library/Authorize.php';
 $appId = WX_APPID;
 $authorize = new lion\weixin\library\Authorize($appId);
 $redirectUrlConfig = [
-	'demo1' => 'http://payment.huangoukeji.com/payment/zx/wxpay/payInterface_jsapi_wx/pay.html',
-	'demo2' => 'http://payment.huangoukeji.com/payment/zx/wxpay/payInterface_jsapi_wx/pay.php',
-	'demo3' => 'http://www.mtws.site//?ct=pay&ac=wxjsapi&oid='.$oid,
-	'demo4' => 'http://payment.huangoukeji.com/?ct=user&ac=uuu&oid='.$money,
-	'demo5' => 'market.huangoukeji.com/?ac=openidpush',
+	'demo1' => 'http://www.mtws.site/payment/zx/wxpay/payInterface_jsapi_wx/pay.html',
+	'demo2' => 'http://www.mtws.site/payment/zx/wxpay/payInterface_jsapi_wx/pay.php',
+	'demo3' => 'http://www.mtws.site/?ct=pay&ac=wxjsapi&oid='.$oid,
+	'demo4' => 'http://www.mtws.site/?ct=user&ac=uuu&oid='.$money,
+	'demo5' => 'www.mtws.site/?ac=openidpush',
 ];
 $authorize->authorizeCodeToUrl($redirectUrlConfig);
